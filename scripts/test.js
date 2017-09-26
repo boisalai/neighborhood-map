2bed650f451bfaf9161f978aa2aae64ae7905fe1
Alain Boisvert <ay.boisvert@gmail.com>
2017-09-25 20:46:59 -0400

    second commit

--- scripts/test.js
+++ scripts/test.js
@@ -1,30 +1,53 @@
 
-	'use strict';
+	"use strict";
 
   var map;
 
   // Create a new blank array for all the listing markers.
   var markers = [];
 
+  var largeInfowindow = null;
+
+  // These are the real estate listings that will be shown to the user.
+  // Normally we'd have these in a database instead.
+  var locations = [
+    {title: "Central Zoo Park", 
+     location: {lat: 40.768236, lng: -73.971566},
+     fact: "The Central Park Zoo is a small 6.5-acre zoo located in Central Park"
+            + " in New York City. It is part of an integrated system of four zoos"
+            + " and the New York Aquarium managed by the Wildlife Conservation..."
+            + " <a href='https://en.wikipedia.org/wiki/Central_Park_Zoo' target='_blank'>Wikipedia</a>"
+    },
+    {title: "Chelsea Loft", 
+     location: {lat: 40.7444883, lng: -73.9949465},
+     fact: "Abcdefg"
+    },
+    {title: "Union Square Open Floor Plan", 
+     location: {lat: 40.7347062, lng: -73.9895759},
+     fact: "Abcdefg"
+    },
+    {title: "East Village Hip Studio", 
+     location: {lat: 40.7281777, lng: -73.984377},
+     fact: "Abcdefg"
+    },
+    {title: "TriBeCa Artsy Bachelor Pad", 
+     location: {lat: 40.7195264, lng: -74.0089934},
+     fact: "Abcdefg"
+    },
+    {title: "Chinatown Homey Space", 
+     location: {lat: 40.7180628, lng: -73.9961237},
+     fact: "Abcdefg"
+    }
+  ];
+
   function initMap() {
     // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('myMap'), {
+    map = new google.maps.Map(document.getElementById("myMap"), {
       center: {lat: 40.7413549, lng: -73.9980244},
       zoom: 13
     });
 
-    // These are the real estate listings that will be shown to the user.
-    // Normally we'd have these in a database instead.
-    var locations = [
-      {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-      {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-      {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-      {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-      {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-      {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-    ];
-
-    var largeInfowindow = new google.maps.InfoWindow();
+    largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
 
     // The following group uses the location array to create an array of markers on initialize.
@@ -32,6 +55,7 @@
       // Get the position from the location array.
       var title = locations[i].title;
       var position = locations[i].location;
+
       // Create a marker per location, and put into markers array.
       var marker = new google.maps.Marker({
         map: map,
@@ -40,14 +64,18 @@
         animation: google.maps.Animation.DROP,
         id: i
       });
+
       // Push the marker to our array of markers.
       markers.push(marker);
+      bounds.extend(markers[i].position);
+      locations[i].marker = marker;
+
       // Create an onclick event to open an infowindow at each marker.
-      marker.addListener('click', function() {
+      marker.addListener("click", function() {
         populateInfoWindow(this, largeInfowindow);
       });
-      bounds.extend(markers[i].position);
     }
+    
     // Extend the boundaries of the map for each marker
     map.fitBounds(bounds);
   }
@@ -59,108 +87,58 @@
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.setContent("<b>" + marker.title + "</b><br>" + locations[marker.id].fact);
       infowindow.open(map, marker);
       // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick',function(){
+      infowindow.addListener("closeclick", function() {
         infowindow.setMarker = null;
       });
     }
   }
 
-    function myFunction() {
-      // This code come from 
-      // https://www.w3schools.com/howto/howto_js_filter_lists.asp
-      var input, filter, ul, li, a, i;
-      input = document.getElementById('myInput');
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("myUL");
-      li = ul.getElementsByTagName('li');
-  
-      // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("a")[0];
-          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-              li[i].style.display = "";
-          } else {
-              li[i].style.display = "none";
-          }
-      }
-  }
-
-  /*
-  // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-  function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
-    this.fullName = ko.computed(function() {
-      return this.firstName() + " " + this.lastName();    
-    }, this);
-  }
-
-  // Activates knockout.js
-  ko.applyBindings(new AppViewModel());
-  */
-
-  function Location(name, address, lat, lng) {
-    var self = this;
-    self.name = name;
-    self.address = address;
-    self.lat = lat;
-    self.lng = lng;
-  }
-
-  // Class to represent a row in the seat reservations grid
-  function SeatReservation(name, initialMeal) {
-    var self = this;
-    self.name = name;
-    self.meal = ko.observable(initialMeal);
-  }
-
   // Overall viewmodel for this screen, along with initial state
-  function ReservationsViewModel() {
+  function NeighborhoodViewModel() {
     var self = this;
 
-    this.abc = ko.observable(null);
-
-    // Non-editable catalog data - would come from the server
-    self.availableMeals = [
-      { mealName: "Standard (sandwich)", price: 0 },
-      { mealName: "Premium (lobster)", price: 34.95 },
-      { mealName: "Ultimate (whole zebra)", price: 290 }
-    ];    
-
-    // Editable data
-    self.seats = ko.observableArray([
-      new SeatReservation("Steve", self.availableMeals[0]),
-      new SeatReservation("Bert", self.availableMeals[0])
-    ]);
-
-    // Operations
-    self.addSeat = function() {
-      self.seats.push(new SeatReservation("", self.availableMeals[0]));
-    }
+    self.locations = ko.observableArray(locations);
 
     self.search = function(data, event) {
-      var input, filter, ul, li, a, i;
-      input = document.getElementById('myInput');
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("myList");
-      li = ul.getElementsByTagName('li');
+      var input = document.getElementById("myInput");
+      var filter = input.value.toUpperCase();
+      var ul = document.getElementById("myList");
+      var li = ul.getElementsByTagName("li");
+
+      var bounds = new google.maps.LatLngBounds();
   
       // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
+      for (var i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
           li[i].style.display = "";
+          // Create an onclick event to open an infowindow at each marker.
+          locations[i].marker.addListener("click", function() {
+            populateInfoWindow(locations[i].marker, largeInfowindow);
+          });
         } else {
           li[i].style.display = "none";
+          markers[i].setMap(null);
         }
       }
     }
+
+    self.showMark = function() {
+      var infowindow = largeInfowindow;
+      infowindow.marker = this.marker;
+      infowindow.setContent("<b>" + this.title + "</b><br>" + this.fact);
+      infowindow.open(map, this.marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener("closeclick", function() {
+        infowindow.setMarker = null;
+      });
+    }    
   }
 
-  ko.applyBindings(new ReservationsViewModel());
+  ko.applyBindings(new NeighborhoodViewModel());
 
   
 
